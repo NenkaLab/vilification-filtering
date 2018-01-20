217781c68b13d28c8a6e124477af3ed5fa83d415
Lumia1108 <jh110821@naver.com>
2018-01-20 23:02:14 +0900

    Upload vf.js

---
+++ src/vf.js
@@ -0,0 +1,15 @@
+/**
+ * vilification-filtering
+ * Project ㅅ
+ * Copyright 2018 to PANN and jhsoft
+ * released under the Apache 2.0 License
+ */
+
+
+
+/*
+ * 욕설 모음 배열
+ * 아는 욕설 다 써주세요.
+ */
+
+var vf = ["씨발" , "개새끼" , "좆" , "씨발년"];
